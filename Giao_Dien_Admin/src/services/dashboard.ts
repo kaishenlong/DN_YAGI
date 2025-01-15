a3bb62ed8e235ea7529d861d5190ac1d9c2ee077
thanhlam200496 <thanhlam200496@gmail.com>
2025-01-15 22:20:04 +0700

    cập nhật giao diện thống kê

--- Giao_Dien_Admin/src/services/dashboard.ts
+++ Giao_Dien_Admin/src/services/dashboard.ts
@@ -1,15 +1,20 @@
 import api from "../config/axios"
 
-export const getallDashboard = async () => {
+export const getallDashboard = async (start_date: string, end_date: string) => {
     try {
-        const {data} = await api.get(`api/dashboard`, {
+        const { data } = await api.get(`api/dashboard`, {
             headers: {
                 Authorization: `Bearer ${localStorage.getItem("token")}`,
-              },
-        })
+            },
+            params: {
+                start_date: start_date,
+                end_date: end_date,
+            },
+        });
         console.log(data);
         return data;
     } catch (error) {
         console.log(error);
     }
-}
+};
+
